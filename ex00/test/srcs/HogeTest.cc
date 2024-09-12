f7cda1471e420b9d6421e9db0189a80c44097a12
tobeshota <Shota.Tobe@keio.jp>
2024-09-12 10:57:48 +0900

    add: add test

---
+++ ex00/test/srcs/HogeTest.cc
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HogeTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+// Basic Addition Test
+TEST(HogeTest, Addition) {
+    int a = 2;
+    int b = 3;
+    EXPECT_EQ(a + b, 5);
+}
---
+++ ex00/test/test.mk
@@ -0,0 +1,6 @@
+test:
+	cmake -S . -B build
+	cmake --build build
+	cd build && ctest --rerun-failed --output-on-failure
+
+.PHONY:	test
