03bcfea8d25d84edec42c8c5ce577603d5e0c68a
tobeshota <Shota.Tobe@keio.jp>
2024-09-13 10:19:41 +0900

    ref: refactor CMakeLists.txt

--- ex00/test/srcs/BureaucratTest.cpp
+++ ex00/test/srcs/BureaucratTest.cpp
@@ -1,7 +1,5 @@
 #include <gtest/gtest.h>
-
 #include <memory>  // スマートポインタ．スコープを抜けると、自動的にメモリが解放されるポインタ
-
 #include "Bureaucrat.hpp"
 
 // Bureaucratがnameを持つ
@@ -11,12 +9,15 @@ TEST(BureaucratTest, nameTest) {
 
   Bureaucrat* NamedByConstructor = new Bureaucrat("NamedByConstructor", 20);
   EXPECT_EQ(NamedByConstructor->getName(), "NamedByConstructor");
+  delete NamedByConstructor;
 
   std::unique_ptr<Bureaucrat> NamedbyMethod = std::make_unique<Bureaucrat>();
   NamedbyMethod->setName("NamedByConstructor");
   EXPECT_EQ(NamedbyMethod->getName(), "NamedByConstructor");
-
-  delete NamedByConstructor;
 }
 
 // Bureaucratがgradeを持つ
+TEST(BureaucratTest, gradeTest) {
+  std::unique_ptr<Bureaucrat> defaultName = std::make_unique<Bureaucrat>();
+  EXPECT_EQ(defaultName->getGrade(), DEFAULT_GRADE);
+}
